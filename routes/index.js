840b1f28d1cd2d5e25e409b4cc677c73074116a9
erdaserhan <erhan209@gmail.com>
2024-09-12 13:44:25 +0200

    update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const app = express();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(request, response, next){
+    response.render('accueil.ejs', {title: "Accueil"});
 });
 
-module.exports = router;
+app.get('/contact', function(request, response, next){
+    response.render('contact.ejs', {title: "Contact"});
+});
+
+app.get('/culture', function(request, response, next){
+    response.render('culture.ejs', {title: "Culture"});
+});
+
+app.get('/galerie', function(request, response, next){
+    response.render('galerie.ejs', {title: "Galerie"});
+});
+
+app.get('/geographie', function(request, response, next){
+    response.render('geographie.ejs', {title: "Geographie"});
+});
+
+app.get('/histoire', function(request, response, next){
+    response.render('histoire.ejs', {title: "Histoire"});
+});
+
+app.get('/liens', function(request, response, next){
+    response.render('liens.ejs', {title: "Liens"});
+});
+
+
+app.use(function(request, response, next) {
+    response.status(404).render('404.ejs', {title: "La page demandée n'existe pas"});
+});
+
+app.listen(8080);
+console.log("Express démarré !");
